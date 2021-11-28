a547c5afa054c3221077da57496974e6efa95aae
Jia <xj.joye@outlook.com>
2021-11-27 23:25:36 -0800

    add email check

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,21 @@
 import React, { Component } from 'react';
-import { Container, Divider, Header, Icon } from 'semantic-ui-react';
+import { Button, Container, Divider, Header, Icon } from 'semantic-ui-react';
 import SignPanel from './SignPanel';
 
 class Home extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      overlayActive: false,
+    }
+  }
+
+  inviteToggle = () => {
+    this.setState({overlayActive: !this.state.overlayActive});
+  }
+
   render() {
+    const { overlayActive } = this.state;
     return (
       <div>
         <Container fluid>
@@ -13,7 +25,14 @@ class Home extends Component {
           <Container textAlign='center' id="page-content" text>
             <Header size='huge' className="page-content-text">A better way to enjoy every day.</Header>
             <p className="page-content-text">Be the first to know when we launch.</p>
-            <SignPanel />
+            <Button basic color='black' size="massive" onClick={this.inviteToggle}>
+              Request an invite
+            </Button>
+            {overlayActive &&
+              <SignPanel
+                overlayActive={overlayActive}
+                onClose={this.inviteToggle}
+              />}
           </Container>
 
           <Divider />
--- src/components/SignPanel.js
+++ src/components/SignPanel.js
@@ -1,27 +1,85 @@
 import React, { Component } from 'react';
-import { Button, Container, Dimmer, Divider, Form, Header, Icon, Segment } from 'semantic-ui-react'
+import { Button, Container, Dimmer, Divider, Form, Header, Input} from 'semantic-ui-react';
+import validator from 'validator';
 
 class SignPanel extends Component {
-  render() {
-    return (
-      <Dimmer id='page-overlay' page>
-        <Container id="sign-panel" text>
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: '',
+      confirmEmail: '',
+      emailError: false,
+      confirmEmailError: false,
+    }
+  }
 
+  closeDimmer = () => {
+    this.props.onClose();
+  }
+
+  validateEmail = (e) => {
+    var email = e.target.value
+    if(email.length > 0) {
+      if (validator.isEmail(email)) {
+        this.setState({emailError: false});
+
+      } else {
+        this.setState({emailError: true});
+      }
+    } else {
+      this.setState({emailError: false});
+    }
+  }
+
+  confirmEmail = (e) => {
+    var email = e.target.value
+    if(email.length > 0) {
+      //一个字符一个字符的对比
+    } else {
+      this.setState({confirmEmailError: false});
+    }
+  }
+
+  render() {
+    const {overlayActive} = this.props;
+    const {emailError, confirmEmailError} = this.state;
+
+    return (
+      <Dimmer active={overlayActive} id='page-overlay' page>
+        <Button icon="close" className="close-layout" size="huge" onClick={this.closeDimmer}/>
+        <Container id="sign-panel" text>
           <Container textAlign='center'>
             <Header as='h4'>Request an invite</Header>
             <Divider />
           </Container>
 
           <Form>
-            <Form.Field>
-              <input placeholder='Full Name' />
-            </Form.Field>
-            <Form.Field>
-              <input placeholder='Email' />
-            </Form.Field>
-            <Form.Field>
-              <input placeholder='Confirm Email' />
-            </Form.Field>
+            <Form.Field
+              control={Input}
+              placeholder='Full name'
+              minLength="3"
+              required
+            />
+            <Form.Field
+              control={Input}
+              placeholder='Email'
+              onChange={this.validateEmail}
+              error={emailError && {
+                content: 'Please enter a valid email address!',
+                pointing: 'above',
+              }}
+              required
+            />
+            <Form.Field
+              control={Input}
+              placeholder='Confirm Email'
+              onChange={this.confirmEmail}
+              error={confirmEmailError && {
+                content: 'Your two email address do not match!',
+                pointing: 'above',
+              }}
+              required
+            />
             <Form.Field>
               <Button type='submit'>Send</Button>
             </Form.Field>
