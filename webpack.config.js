dc15b7a022049410e0d20a057534f4dc1bf0ec31
Jia <xj.joye@outlook.com>
2021-11-28 21:42:30 -0800

    deploy

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,7 @@
 const webpack = require('webpack');
+const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 const port = process.env.PORT || 8080;
 
@@ -7,7 +9,8 @@ module.exports = {
   mode: 'development',
   entry: './src/index.js',
   output: {
-    filename: 'bundle.[hash].js'
+    // filename: 'bundle.[hash].js',
+    path: path.resolve(__dirname, 'dist'),
   },
   devtool: 'inline-source-map',
   module: {
@@ -37,7 +40,8 @@ module.exports = {
   plugins: [
     new HtmlWebpackPlugin({
       template: 'public/index.html',
-    })
+    }),
+    new CleanWebpackPlugin()
   ],
   devServer: {
     host: 'localhost',
